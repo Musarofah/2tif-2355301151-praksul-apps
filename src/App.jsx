4697179e52f35d3affeb24cd79068a61b1633358
krupukcabe <sarofahmu@gmail.com>
2025-06-24 12:53:13 +0700

    ppp

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-// src/App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./pertemuan-3/components/main"; // ✅ betul jika nama file memang "main.jsx"
+// // src/App.jsx
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Main from "./pertemuan-3/components/main"; // ✅ betul jika nama file memang "main.jsx"
 
-function Home() {
-  return <h1>Beranda Aplikasi</h1>;
-}
+// function Home() {
+//   return <h1>Beranda Aplikasi</h1>;
+// }
 
-function NotFound() {
-  return <h1>404 - Halaman tidak ditemukan</h1>;
-}
+// function NotFound() {
+//   return <h1>404 - Halaman tidak ditemukan</h1>;
+// }
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pertemuan-3" element={<Main />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
-}
+// export default function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/pertemuan-3" element={<Main />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
