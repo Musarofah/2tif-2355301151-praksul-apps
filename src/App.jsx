46b1892a9c49e97017f8818c4b7448e2c2fe8e2c
GGBE123 <android.aya14@gmail.com>
2025-06-24 07:54:46 +0700

    praksul

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,35 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard";
-import './assets/tailwind.css'
-
-// import Orders from "./pages/Orders";
-// import Customers from "./pages/customers";
-// import NotFound from "./pages/NotFound";
-import Error400 from "./pages/Error400";
-import Error401 from "./pages/Error401";
-import Error403 from "./pages/Error403";
-import Loading from "./components/Loading";
-import ErrorPage from './components/ErrorPage';
-import GuestLayout from './layouts/GuestLayout';
-// import GuestLayout from './layouts/GuestLayout';
-
-// import MainLayout from "./layouts/MainLayout";
-// import AuthLayout from "./layouts/AuthLayout";
-// import Login from "./pages/auth/Login";
-// import Register from "./pages/auth/Register";
-// import Forgot from "./pages/auth/Forgot";
-import React, { lazy, Suspense } from 'react';
-const MainLayout = lazy(() => import('./layouts/MainLayout'));
-const AuthLayout = lazy(() => import('./layouts/AuthLayout'));
-
-
-const Orders = lazy(() => import('./pages/orders'));
-const Customers = lazy(() => import('./pages/customers'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-const Login = lazy(() => import('./pages/auth/Login'));
-const Register = lazy(() => import('./pages/auth/Register'));
-const Forgot = lazy(() => import('./pages/auth/Forgot'));
-const Dashboard = React.lazy(() => import("./pages/Dashboard"))
-const Users = lazy(() => import('./pages/Users'));
-const Note = lazy(() => import('./pages/Note'));
-const Guest = lazy(() => import('./pages/guest/Guest'));
-
-const KetersediaanP = lazy(() => import('./pages/KetersediaanP'));
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/error-400" element={<Error400 />} />
-            <Route path="/error-401" element={<Error401 />} />
-            <Route path="/error-403" element={<Error403 />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="/ketersediaanp" element={<KetersediaanP />} />
-            <Route path="/note" element={<Note />} />
-          </Route>
-
-          <Route element={<AuthLayout />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot" element={<Forgot />} />
-          </Route>
-
-          <Route element={<GuestLayout />}>
-            <Route path="/guest" element={<Guest />} />
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
-
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
